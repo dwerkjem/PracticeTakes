7b1597c99d7f1615826b0091b5f3dd35701bb4cd
Derek Neilson <42622294+dwerkjem@users.noreply.github.com>
2026-07-16 01:35:33 +0000

    Add tuner component interface

---
+++ src/TunerComponent.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+#include <array>
+#include <vector>
+
+class TunerComponent final : public juce::AudioAppComponent,
+                             private juce::Timer
+{
+public:
+    TunerComponent();
+    ~TunerComponent() override;
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    void paint(juce::Graphics& graphics) override;
+    void resized() override;
+
+private:
+    static constexpr int fifoCapacity = 32768;
+    static constexpr int analysisSize = 4096;
+
+    void timerCallback() override;
+    void drainAudioFifo();
+    [[nodiscard]] double detectPitch() const;
+    void updateNote(double frequency);
+
+    juce::AbstractFifo audioFifo { fifoCapacity };
+    std::array<float, fifoCapacity> fifoBuffer {};
+    std::array<float, analysisSize> analysisBuffer {};
+
+    std::vector<float> correlation;
+    double currentSampleRate = 44100.0;
+    double displayedFrequency = 0.0;
+    double displayedCents = 0.0;
+    juce::String displayedNote { "--" };
+    float inputLevel = 0.0f;
+    bool hasSignal = false;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TunerComponent)
+};
